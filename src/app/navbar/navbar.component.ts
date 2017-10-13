f7711075dc9918f00a9bd86ce8bb1cbb50b14325
Unknown <ernstkaese@gmail.com>
2017-10-13 12:07:02 +0200

    Look and Feel changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -21,7 +21,6 @@ export class NavbarComponent implements OnInit {
 
   onResize(event) {
     this.width = window.innerWidth - 75;
-    console.log(this.width);
     // tslint:disable-next-line:max-line-length
     this.fbLink = this.sanitizer.bypassSecurityTrustResourceUrl('https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FDreamValleyHandmadeSoap%2F&tabs=timeline&width=' + this.width + '&height=450&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId');
   }
